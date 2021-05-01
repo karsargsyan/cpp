6b4201197e930a3cd03265316634c12eba1eee32
Your Name <you@example.com>
2021-05-01 20:36:45 +0400

    first commit

---
+++ lesson2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int age;
+int count;
+cout <<"Enter a number";
+cin >>count;
+cout<< "Enter a age";
+cin>> age; 
+cout<< "count= "<<count <<endl;
+cout<<"Age= "<<age; 
+
+
+return 0;
+
+}
